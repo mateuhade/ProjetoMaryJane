69597d092bf42b3ce17eaf997be36d6aa7822d40
mateuhade <tartarohades552@gmail.com>
2026-03-28 03:29:59 -0300

    projeto mary jane

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.querySelector("body")
+let sapato = document.querySelector(".sapato1")
+
+function mudarCor(cor, sapatoo) {
+    body.style.background = cor
+    sapato.src = sapatoo
+}
